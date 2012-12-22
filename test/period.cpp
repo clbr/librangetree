9a4fce4cb4001e5e1931bddc569ee2fcfebba9c3
Lauri Kasanen <curaga@operamail.com>
2012-12-22 20:39:20 +0200

    Instead of 2^32, let's do 250k

--- test/period.cpp
+++ test/period.cpp
@@ -1,14 +1,15 @@
 #include "test.h"
-#include <limits.h>
 
 int main() {
+
+	const u32 MAX = 500;
 
 	rangetree<u16, u16> tree;
 
 	u16 x, y;
 
-	for (x = 0; x < USHRT_MAX; x++) {
-		for (y = 0; y < USHRT_MAX; y++) {
+	for (x = 0; x < MAX; x++) {
+		for (y = 0; y < MAX; y++) {
 			tree.add(x, y, 0);
 		}
 	}
@@ -17,8 +18,8 @@ int main() {
 
 	// With points in every coordinate, we can easily calculate the counts
 
-	for (x = 10; x < USHRT_MAX; x += 10) {
-		for (y = 10; y < USHRT_MAX; y += 10) {
+	for (x = 10; x < MAX; x += 10) {
+		for (y = 10; y < MAX; y += 10) {
 			const u32 count = tree.count(1, x, 1, y);
 			const u32 calc = x*y;
 
