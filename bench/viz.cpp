bb641fd80b526e48c12456335cfad3f0650b465f
Lauri Kasanen <curaga@operamail.com>
2012-12-28 14:58:42 +0200

    Add visualizing app

---
+++ bench/viz.cpp
@@ -0,0 +1,17 @@
+#define LR_VISUALIZE
+#include <ranget.h>
+
+int main() {
+
+	rangetree<u16,u16> tree(10, 10);
+
+	tree.add(1, 0, 0);
+	tree.add(10, 0, 0);
+	tree.add(100, 0, 0);
+
+	tree.finalize();
+
+	tree.visualize();
+
+	return 0;
+}
